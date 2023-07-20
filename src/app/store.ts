b22ecd503c7c6fff67ce0d5ef6df700e2bd86da5
Prantosh Biswas <pbiswas1007@gmail.com>
2023-07-20 20:59:50 +0530

    Add Contributors in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import snippetReducer from '../features/snippets/snippetsSlice';
+import contributorReducer from '../features/contributors/contributorsSlice'
 
 const store = configureStore({
   reducer: {
     snippets: snippetReducer,
+    contributors: contributorReducer
   },
 });
 
